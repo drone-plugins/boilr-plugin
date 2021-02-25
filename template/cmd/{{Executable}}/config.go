7b74a23c05febaaa62c683abfda26aa0f22abb56
Don <don.j.olmstead@gmail.com>
2021-02-25 12:50:59 -0800

    Update copyright to 2021

--- template/cmd/{{Executable}}/config.go
+++ template/cmd/{{Executable}}/config.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, the Drone Plugins project authors.
+// Copyright (c) 2021, the Drone Plugins project authors.
 // Please see the AUTHORS file for details. All rights reserved.
 // Use of this source code is governed by an Apache 2.0 license that can be
 // found in the LICENSE file.
